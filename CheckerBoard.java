ae76789f0b9974f2bbbb9744d3cfadc24b1c3e31
Chandika-04 <80723626+Chandika-04@users.noreply.github.com>
2021-05-31 20:00:36 -0700

    Update CheckerBoard.java

--- CheckerBoard.java
+++ CheckerBoard.java
@@ -2,12 +2,17 @@ import java.util.*;
 public class CheckerBoard{
 	String[][] board;
 	ArrayList<Piece> allPieces;
+	int size;
 
-	public CheckerBoard(int size){
-		board = new String[size][size];
+	public CheckerBoard(){
+	}
+
+	public void setSize(int s){
+		size = s;
+		board = new String[s][s];
 		allPieces = new ArrayList<Piece>();
 		for (int i = 0; i < 3; i++){
-			for (int j = 0; j < size; j++){
+			for (int j = 0; j < s; j++){
 				if(i % 2 == 0){
 					if(j % 2 == 1){
 						Piece a = new Piece(i, j, "r");
@@ -22,8 +27,8 @@ public class CheckerBoard{
 
 			}
 		}
-		for (int i = size - 3; i < size; i++){
-			for (int j = 0; j < size; j++){
+		for (int i = s - 3; i < s; i++){
+			for (int j = 0; j < s; j++){
 				if(i % 2 == 0){
 					if(j % 2 == 1){
 						Piece a = new Piece(i, j, "b");
@@ -40,25 +45,81 @@ public class CheckerBoard{
 	}
 
 	public void updateBoard(){
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board[0].length; j++){
+				board[i][j] = null;
+			}
+		}
 		for(Piece x : allPieces){
 			board[x.getRow()][x.getCol()] = x.getSymbol();
 		}
 	}
 
 	public String toString(){
-		String result = "\n+---+---+---+---+---+---+---+---+\n|";
+		int rowcount = size;
+		String result = "\n  +---+---+---+---+---+---+---+---+\n";
 		for (int i = 0; i < board.length; i++){
+			result += rowcount + " ";
+			rowcount--;
 			for(int j = 0; j < board[0].length; j++){
 				if (board[i][j] != null){
-					result += " " + board[i][j] + " |";
+					result += "| " + board[i][j] + " ";
 				} else {
-					result += "   |";
+					result += "|   ";
 				}
 			}
-			result += "\n+---+---+---+---+---+---+---+---+";
-			result +="\n|";
+			result += "|\n  +---+---+---+---+---+---+---+---+\n";
 		}
+		result += "    1   2   3   4   5   6   7   8  ";
 		return result;
+	}
+
+	public void moveUpRight(int col, int row){
+		row = size - row;
+		col--;
+		for(Piece x : allPieces){
+			if(col == x.getCol() && row == x.getRow()){
+				x.moveUp();
+				x.moveRight();
+				break;
+			}
+		}
+	}
+
+	public void moveUpleft(int col, int row){
+		row = size - row;
+		col--;
+		for(Piece x : allPieces){
+			if(col == x.getCol() && row == x.getRow()){
+				x.moveUp();
+				x.moveLeft();
+				break;
+			}
+		}
+	}
+
+	public void moveDownleft(int col, int row){
+		row = size - row;
+		col--;
+		for(Piece x : allPieces){
+			if(col == x.getCol() && row == x.getRow()){
+				x.moveDown();
+				x.moveLeft();
+				break;
+			}
+		}
+	}
+
+	public void moveDownRight(int col, int row){
+		row = size - row;
+		col--;
+		for(Piece x : allPieces){
+			if(col == x.getCol() && row == x.getRow()){
+				x.moveDown();
+				x.moveRight();
+				break;
+			}
+		}
 	}
 	
 }
